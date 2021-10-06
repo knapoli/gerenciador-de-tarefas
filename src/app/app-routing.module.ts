ad2c782911bef91906b0af0ab381e057c5e57367
Thiago Napoli <71907294+knapoli@users.noreply.github.com>
2021-10-06 14:24:19 -0300

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { TarefaRoutes } from './tarefas/tarefa-routing.module';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/tarefas/listar',
-    pathMatch: 'full'
-  },
-  ...TarefaRoutes
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
